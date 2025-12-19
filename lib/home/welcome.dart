2fb50d62221aeaefff449bb91f5cf610227be731
Ülari Ainjärv <ylari.ainjarv@gmail.com>
2025-12-19 11:08:14 +0200

    Done

--- lib/home/welcome.dart
+++ lib/home/welcome.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
 import 'package:example/common/header.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:geolocator/geolocator.dart';
+import 'package:intl/intl.dart';
 
 class Welcome extends StatefulWidget {
   const Welcome({super.key});
@@ -11,27 +14,36 @@ class Welcome extends StatefulWidget {
 }
 
 class _WelcomeState extends State<Welcome> {
+  bool _loading = false;
+  Position? _location;
+
   @override
   void initState() {
     super.initState();
+    _loading = true;
     _getLocation().then((position) {
       debugPrint(position.toString());
-      _load(position.latitude.toString(), position.longitude.toString());
+      // _load(position.latitude.toString(), position.longitude.toString());
+      setState(() {
+        _location = position;
+        _loading = false;
+      });
     });
   }
 
-  void _load(String lat, String lon) async {
-    await http
-        .get(
-          Uri.https('api.met.no', 'weatherapi/locationforecast/2.0/compact', {
-            'lat': lat,
-            'lon': lon,
-          }),
-          headers: {'User-Agent': 'MyApp/1.0 (ylari.ainjarv@gmail.com)'},
-        )
-        .then((response) {
-          debugPrint(response.body);
-        });
+  Future<List<dynamic>> _load(String lat, String lon) async {
+    final response = await http.get(
+      Uri.https('api.met.no', 'weatherapi/locationforecast/2.0/compact', {
+        'lat': lat,
+        'lon': lon,
+      }),
+      headers: {'User-Agent': 'MyApp/1.0 (ylari.ainjarv@gmail.com)'},
+    );
+    if (response.statusCode != 200) {
+      throw Exception('HTTP error ${response.statusCode}');
+    }
+    final data = jsonDecode(response.body);
+    return data['properties']['timeseries'];
   }
 
   Future<Position> _getLocation() async {
@@ -47,11 +59,61 @@ class _WelcomeState extends State<Welcome> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: Header(title: 'Avaleht'),
-      body: Center(
-        child: Image.asset('assets/ylari.jpeg', width: 90, height: 90),
-      ),
-    );
+    return _loading
+        ? Center(child: CircularProgressIndicator())
+        : Scaffold(
+            appBar: Header(title: 'Avaleht'),
+            body: FutureBuilder(
+              future: _load(
+                _location!.latitude.toString(),
+                _location!.longitude.toString(),
+              ),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                if (snapshot.hasError) {
+                  return Center(child: Text('Error ${snapshot.error}'));
+                }
+                final items = snapshot.data as List<dynamic>;
+                return ListView.builder(
+                  itemCount: items.length,
+                  itemBuilder: (context, index) {
+                    final item = items[index];
+                    final time = DateFormat(
+                      'dd.MM.yyyy HH:mm:ss',
+                    ).format(DateTime.parse(item['time']).toLocal());
+                    final details = item['data']['instant']['details'];
+                    final temperature = details['air_temperature'];
+                    final humidity = details['relative_humidity'];
+                    final pressure = details['air_pressure_at_sea_level'];
+                    final wind = details['wind_speed'];
+                    String next = '';
+                    if (item['data']['next_1_hours'] != null) {
+                      next =
+                          item['data']['next_1_hours']['summary']['symbol_code'] ??
+                          '';
+                    }
+                    return ListTile(
+                      leading: Text('${index + 1}'),
+                      title: Text(
+                        time,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text(
+                        'Temperature: $temperature °C\n'
+                        'Humidity: $humidity %\n'
+                        'Pressure: $pressure hPa\n'
+                        'Wind speed: $wind m/s',
+                      ),
+                      trailing: next != ''
+                          ? Image.asset('assets/symbols/$next.png')
+                          : null,
+                    );
+                  },
+                );
+              },
+            ),
+          );
   }
 }
